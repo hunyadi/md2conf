b1bf3057aa4e994b230f14456106462436043d32
Levente Hunyadi <levente.hunyadi@instructure.com>
2024-01-26 15:41:41 +0100

    Publish new version to PyPI

--- md2conf/__init__.py
+++ md2conf/__init__.py
@@ -5,9 +5,9 @@ Parses Markdown files, converts Markdown content into the Confluence Storage For
 Confluence API endpoints to upload images and content.
 """
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 __author__ = "Levente Hunyadi"
-__copyright__ = "Copyright 2022-2023, Levente Hunyadi"
+__copyright__ = "Copyright 2022-2024, Levente Hunyadi"
 __license__ = "MIT"
 __maintainer__ = "Levente Hunyadi"
 __status__ = "Production"
