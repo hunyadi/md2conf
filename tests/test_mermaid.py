e8ed22dfb16cebc663b19379aafac96fac63d43d
Levente Hunyadi <hunyadi@gmail.com>
2025-09-19 11:37:29 +0200

    Fix an issue in unit tests

--- tests/test_mermaid.py
+++ tests/test_mermaid.py
@@ -8,11 +8,9 @@ Copyright 2022-2025, Levente Hunyadi
 
 import logging
 import os
-import shutil
 import unittest
-from pathlib import Path
+import xml.etree.ElementTree as ET
 
-from md2conf.extra import override
 from md2conf.mermaid import has_mmdc, render_diagram
 from tests.utility import TypedTestCase
 
@@ -32,29 +30,10 @@ graph TD
 @unittest.skipUnless(has_mmdc(), "mmdc is not available")
 @unittest.skipUnless(os.getenv("TEST_MERMAID"), "mermaid tests are disabled")
 class TestMermaidRendering(TypedTestCase):
-    out_dir: Path
-
-    @override
-    def setUp(self) -> None:
-        self.maxDiff = 1024
-
-        test_dir = Path(__file__).parent
-        parent_dir = test_dir.parent
-
-        self.out_dir = test_dir / "output"
-        self.sample_dir = parent_dir / "sample"
-        os.makedirs(self.out_dir, exist_ok=True)
-
-    @override
-    def tearDown(self) -> None:
-        shutil.rmtree(self.out_dir)
-
     def test_render_simple_svg(self) -> None:
-        svg = render_diagram(MERMAID_SOURCE, output_format="svg").decode()
-
-        self.assertIn("transform=", svg)
-        self.assertIn("translate(", svg)
-        self.assertIn("<rect height=", svg)
+        svg = render_diagram(MERMAID_SOURCE, output_format="svg")
+        root = ET.fromstring(svg)
+        self.assertTrue(root.tag.lower() == "svg" or root.tag.endswith("}svg"))
 
     def test_render_simple_png(self) -> None:
         png = render_diagram(MERMAID_SOURCE)
